92ae3ba63c1bd675274653b1cec045e02c5eb371
Prayut Chaisook <prayutchaisook@gmail.com>
2020-06-27 00:39:57 +0700

    Kick Off

---
+++ src/App/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+export default App;
